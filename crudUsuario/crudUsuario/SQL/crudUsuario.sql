bdd26732583c4aa9dc81fb95057e097f121c4075
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2026-01-19 12:47:58 +0100

    Add roles and usuario_roles tables to support user role management

--- crudUsuario/crudUsuario/SQL/crudUsuario.sql
+++ crudUsuario/crudUsuario/SQL/crudUsuario.sql
@@ -16,8 +16,26 @@ email varchar(100) unique not null,
 password varchar(200) not null,
 
 activo TINYINT(1) DEFAULT 1
+);
+
+
+CREATE TABLE roles(
+                      id int primary key auto_increment not null ,
+                      nombre_rol varchar(100)unique not null
+);
+
+CREATE TABLE usuario_roles(
+                              id int primary key auto_increment not null ,
+                                id_usuario int,
+                                id_rol int,
+                                FOREIGN KEY (id_usuario) references usuario(id),
+                                FOREIGN KEY (id_rol) references roles(id)
+
+
 
 );
+
+
 
 insert into usuario ( nombre, apellidos, nombre_usuario, email, password) values (
                                                                   'Rafa', 'Medina Ayuso', 'ikran','rafa@gmail.com', '$2a$12$CzdU7n41SWsBnlPVerrdu.rAHZGeeYxXqkZYyyraM0YxrT6ry4b.2'
